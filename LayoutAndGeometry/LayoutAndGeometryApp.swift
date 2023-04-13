d4d3c190c0c7e575c92e4b469be9f8f7884b1809
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-13 23:50:44 +0400

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by aplle on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
