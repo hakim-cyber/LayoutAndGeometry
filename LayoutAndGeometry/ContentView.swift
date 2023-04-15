77b8b6e6d09bd405f65fbf8fd8aaeaf4ad23d5d7
Hakim Aliyev <115820565+hakim-cyber@users.noreply.github.com>
2023-04-16 00:56:54 +0400

    Solutions for checkpoints 1,2,3

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -6,23 +6,92 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
+struct OuterView:View{
     var body: some View {
-        VStack(alignment: .leading){
-           
-            ForEach(0..<20){position in
-                Text("Number: \(position)")
-                    .alignmentGuide(.leading){_ in
-                        Double(position) * -10
+        VStack{
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
+        }
+    }
+}
+struct InnerView:View{
+    var body: some View {
+        HStack{
+            Text("left")
+            GeometryReader{geo in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Glob cent :\(geo.frame(in: .global).midX) x \(geo.frame(in: .global).midY)")
+                        print("Local cent :\(geo.frame(in: .local).midX) x \(geo.frame(in: .local).midY)")
+                        print("cust cent :\(geo.frame(in: .named("Custom")).midX) x \(geo.frame(in: .named("Custom")).midY)")
                     }
                 
             }
+            .background(.orange)
+               
+            Text("Right")
         }
-        .background(.red)
-        .frame(width: 400,height: 400)
-        .background(.blue)
     }
+}
+
+
+
+
+
+
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(getColor(for: geo.frame(in: .global).minY, full: fullView.frame(in: .global).maxY))
+                            .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .opacity(getOpacity(for: geo.frame(in: .global).minY))
+                            .scaleEffect(getScale(for: geo.frame(in: .global).minY) )
+                            
+                    }
+                    .frame(height: 40)
+                }
+               
+            }
+        }
+    }
+    func getOpacity(for height:CGFloat)-> Double{
+        if height > 200{
+            return 1.0
+        
+        }
+        return Double(abs(height / 200))
+        
+        
+    }
+    
+    func getScale(for height:CGFloat)-> Double{
+        if height > 200{
+            return 1.0
+        }else if height < 100{
+            return 0.5
+        }
+        return Double(abs(height / 200))
+    }
+    func getColor (for height:CGFloat,full :CGFloat)-> Color{
+        let index = Double(abs(height / full * 1.5 ))
+        return withAnimation {
+            Color(hue:index * 0.7,saturation:index * 1.2,brightness:index * 1.2)
+        }
+       
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
